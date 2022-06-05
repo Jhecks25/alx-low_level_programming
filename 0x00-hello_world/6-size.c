9e8fc382837e8c984b145010031eb57a70f599a5
John <johnyhecks@gmail.com>
2022-06-05 10:47:26 -0700

    Hello World task 6 sizeoff

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int i;
 long long int d;
 long int a;
 float f;
-printf("The size of a char is: %lu byte(s).\n", (unsigned long) sizeof(c));
-printf("The size of an int is: %lu byte(s).\n", (unsigned long) sizeof(i));
-printf("The size of a long int is: %lu byte(s).\n", (unsigned long) sizeof(a));
-printf("The size of a long long int is: %lu byte(s).\n", (unsigned long) sizeof(d));
-printf("The size of a float is: %lu byte(s).\n", (unsigned long) sizeof(f));
+printf("The size of a char: %lu byte(s).\n", (unsigned long) sizeof(c));
+printf("The size of an int: %lu byte(s).\n", (unsigned long) sizeof(i));
+printf("The size of a long int: %lu byte(s).\n", (unsigned long) sizeof(a));
+printf("The size of a long long int: %lu byte(s).\n", (unsigned long) sizeof(d));
+printf("The size of a float: %lu byte(s).\n", (unsigned long) sizeof(f));
 return (0);
 }
