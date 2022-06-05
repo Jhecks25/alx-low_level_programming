5744b45dcf0506c1840abee9bc3c2e8cea4a7464
John <johnyhecks@gmail.com>
2022-06-05 00:11:26 -0700

    Hello World task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/*using puts instead of printf*/
+/**
+ *using puts instead of printf
+*/
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 	getchar();
