bde01f382227bd599e4d7efbc5c81e4982a171ff
John <johnyhecks@gmail.com>
2022-06-05 08:19:46 -0700

    Hello World task 6 sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,9 +9,9 @@ char c;
 int i;
 double d;
 float f;
-printf("The size of an char is: %lu.\n", (unsigned long) sizeof(c));
+printf("The size of a char is: %lu.\n", (unsigned long) sizeof(c));
 printf("The size of an int is: %lu.\n", (unsigned long) sizeof(i));
 printf("The size of a long int is: %lu.\n", (unsigned long) sizeof(d));
-printf("The size of a long int is: %lu.\n", (unsigned long) sizeof(f));
+printf("The size of a float is: %lu.\n", (unsigned long) sizeof(f));
 return (0);
 }
