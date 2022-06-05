ca7259a42e1c5f9b6ec2ea917e44638942dd4d91
John <johnyhecks@gmail.com>
2022-06-05 00:35:05 -0700

    Hello World task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
+ * int main - using puts
  *Return: 0 value of main
 */
 int main(void)
-/**
- *int main - using puts
-*/
 {puts("\"Programming is like building a multilingual puzzle");
 	getchar();
 	return (0);
