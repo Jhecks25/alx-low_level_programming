8d260f479ab868b1ab8949894d8203312bb274c5
John <johnyhecks@gmail.com>
2022-06-05 08:04:23 -0700

    Hello World task 6 sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- *maim - Printing sizeof
+ *main - Printing sizeof
  *Return: 0 Always success
  */
 int main(void)
 {
+char c;
 int i;
 double d;
-char c;
+printf("The size of an char is: %lu.\n", (unsigned long) sizeof(c));
 printf("The size of an int is: %lu.\n", (unsigned long) sizeof(i));
-printf("The size of an int is: %lu.\n", (unsigned long) sizeof(d));
-printf("The size of an int is: %lu.\n", (unsigned long) sizeof(c));
+printf("The size of a long int is: %lu.\n", (unsigned long) sizeof(d));
 return (0);
 }
