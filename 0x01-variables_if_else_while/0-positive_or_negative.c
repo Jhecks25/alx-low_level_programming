e195fe93fc24117ec05cf3c0f72f482a62c653e8
John <johnyhecks@gmail.com>
2022-06-05 19:30:48 -0700

    Printing random positive and negattive numbers with if_else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**main - random numbers
- *
- * *Return: 0 Always success
- *
- * */
-
+/*main - random numbers
+ *Return: 0 Always success
+ */
 int main(void)
-
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n > 0){
+if(n > 0)
+{
 printf("The numbers are positive");
 }
-else{
+else
+{
 printf("The numbers are negative");
 }
 return (0);
