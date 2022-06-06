c2a1319f30800a8f9c62cf24c87b2d1b701b22e8
John <johnyhecks@gmail.com>
2022-06-05 21:35:08 -0700

    Printing last digit of random numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, LastDigit);
 }
 else if (n < 6 && n != 0)
 {
-printf("Last digit of %d is %d and is less than 6\n and not 0", n, LastDigit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, LastDigit);
 }
 else
 {
