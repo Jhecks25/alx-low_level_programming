8d60f61d81de30c8d1c1bf3851ec4285baa7b0d3
John <johnyhecks@gmail.com>
2022-06-06 19:11:53 -0700

    Printing alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - looping alphabets
+*Return: 0 Always success
+*/
+int main(void)
+{
+char b;
+for (b = 'z'; b >= 'z'; b++)
+{
+putchar(b);
+}
+putchar('\n');
+return (0);
+}
