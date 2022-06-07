7027dce7f0f78b1cf12c1430a5e3ee248bbd7787
John <johnyhecks@gmail.com>
2022-06-06 19:43:36 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Looping numbers
+ * Return: 0 always success
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+putchar((i % 10) + '0');
+{
+if (i == 9) continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
