9f3ef35272bf49ac18513c59741904e0da9f694f
John <johnyhecks@gmail.com>
2022-06-16 13:42:16 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main"
+#include <stdio>
+
+/**
+ * main - check
+ * strcat- concatenates 2 strings
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char s1[98] = "Hello ";
+        char s2[] = "World\n";
+        
+	strcat(s1, s2);
+
+	return (0);
