c4dd0ce39d58f7ff7e2103a9121e65acf50b5b1b
John <johnyhecks@gmail.com>
2022-06-28 15:59:09 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *
+ *
+ */
+
+int main(int argc, char* argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+		
+	}
+}
+
